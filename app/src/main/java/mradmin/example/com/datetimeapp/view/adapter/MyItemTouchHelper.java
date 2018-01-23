a35656abafd49efa606dcf9256c9de5a60e685b2
yks <vladimir64kor@gmail.com>
2018-01-23 18:11:24 +0400

    item touch helper back draw on remove

--- app/src/main/java/mradmin/example/com/datetimeapp/view/adapter/MyItemTouchHelper.java
+++ app/src/main/java/mradmin/example/com/datetimeapp/view/adapter/MyItemTouchHelper.java
@@ -1,11 +1,23 @@
 package mradmin.example.com.datetimeapp.view.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+import mradmin.example.com.datetimeapp.R;
 
 public class MyItemTouchHelper extends ItemTouchHelper.Callback {
 
     private ItemTouchHelperAdapter adapter;
+    private Paint p = new Paint();
 
     public interface ItemTouchHelperAdapter {
         void onItemMoved(int fromPosition, int toPosition);
@@ -47,4 +59,52 @@ public class MyItemTouchHelper extends ItemTouchHelper.Callback {
 
     }
 
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        Bitmap icon;
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+            View itemView = viewHolder.itemView;
+            float height = (float) itemView.getBottom() - (float) itemView.getTop();
+            float width = height / 3;
+
+            p.setColor(Color.parseColor("#009ad2"));
+
+            if (dX > 0) {
+
+                RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX, (float) itemView.getBottom());
+                c.drawRect(background, p);
+
+                Drawable drawable = ContextCompat.getDrawable(recyclerView.getContext(), R.drawable.ic_delete_white);
+
+                icon = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+                Canvas canvas = new Canvas(icon);
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+
+                RectF icon_dest = new RectF((float) itemView.getLeft() + width, (float) itemView.getTop() + width, (float) itemView.getLeft() + 2 * width, (float) itemView.getBottom() - width);
+                c.drawBitmap(icon, null, icon_dest, p);
+
+            } else {
+
+                RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                c.drawRect(background, p);
+
+                Drawable drawable = ContextCompat.getDrawable(recyclerView.getContext(), R.drawable.ic_delete_white);
+
+                icon = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+                Canvas canvas = new Canvas(icon);
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+
+                RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                c.drawBitmap(icon, null, icon_dest, p);
+
+            }
+        }
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
 }
